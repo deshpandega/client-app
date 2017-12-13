cad67503221dce6768b3e1e6bb11e9a3818757fe
sb <bhole.s@husky.neu.edu>
2017-12-13 18:53:39 -0500

    My Profile page UI change4

--- src/app/editProfile/editProfile.component.ts
+++ src/app/editProfile/editProfile.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'edit-profile',
@@ -6,6 +6,7 @@ import {Component} from "@angular/core";
   styleUrls: ['./editProfile.component.css']
 })
 
-export class EditProfileComponent{
+export class EditProfileComponent  {
+
 
 }
