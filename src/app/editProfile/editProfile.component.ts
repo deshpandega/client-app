9410ed671c76078092c668c610f30751aed18b23
sb <bhole.s@husky.neu.edu>
2017-12-14 17:14:40 -0500

    Edit Profile changes

--- src/app/editProfile/editProfile.component.ts
+++ src/app/editProfile/editProfile.component.ts
@@ -8,5 +8,36 @@ import {Component} from '@angular/core';
 
 export class EditProfileComponent  {
 
+  currentDiv= 'profile' ;
+  managePayment(){
+    this.currentDiv = 'payment';
+    console.log(this.currentDiv + ' is the current value');
+  }
+  openDiv() {
+    this.currentDiv = 'profile';
+    console.log( ' ------ ' + this.currentDiv);
+    // if (activityName === 'EditProfile') {
+    //   this.currentDiv = 'profile';
+    // } else {
+    //   this.currentDiv = 'falafal';
+    // }
+    // var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    // tabcontent = document.getElementsByClassName("tabcontent");
+    // for (i = 0; i < tabcontent.length; i++) {
+    //   tabcontent[i].style.display = "none";
+    // }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    // tablinks = document.getElementsByClassName("tablinks");
+    // for (i = 0; i < tablinks.length; i++) {
+    //   tablinks[i].className = tablinks[i].className.replace(" active", "");
+    // }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    // document.getElementById(activityName).style.display = "block";
+    // evt.currentTarget.className += " active";
+  }
 
 }
