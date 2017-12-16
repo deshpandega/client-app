fd465af40d9b686a050df6d21f3169044754d1c0
Mitali Ichalkaranje <ichalkaranje.m@husky.neu.edu>
2017-12-16 03:40:28 -0500

    create event related

--- src/app/event-create/event-create.component.ts
+++ src/app/event-create/event-create.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from "@angular/core";
 import {SharedService} from "../shared/shared.service";
+import {Router} from "@angular/router";
 import {FormGroup, FormBuilder, FormControl, Validators} from "@angular/forms";
 import {Http, Response, RequestOptions, Headers} from "@angular/http";
 import { Event } from "../shared/event.model";
+import { User } from "../shared/user.model";
 
 
 @Component({
@@ -16,10 +18,11 @@ export class EventCreateComponent implements OnInit{
     loading:Boolean = false;
     event:Event;
     hobbies:any;
+    user:User;
     errorUserLogin:string;
   
     //Token to check authentication of users
-    token:string;
+    public token:any;
   
     nameControl;
     venueControl;
@@ -29,9 +32,11 @@ export class EventCreateComponent implements OnInit{
     eventEndTimeControl;
     entryFeeControl;
     // Constructor to inject things
-    constructor(private formBuilder: FormBuilder, public http: Http, private _sharedService: SharedService){
+    constructor(private router:Router,private formBuilder: FormBuilder, public http: Http, private _sharedService: SharedService){
+      this.token=this._sharedService.token;
+      this.authenticateToken();
       this.buildForm();
-     //this.createEventMethod();
+      this.loadHobbies();
     }
   
     // InIt method makes things ready when component is loaded
@@ -53,12 +58,6 @@ export class EventCreateComponent implements OnInit{
         eventStartTime:this.formBuilder.control('', Validators.required),
         eventEndTime:this.formBuilder.control('', Validators.required),
         entryFee:this.formBuilder.control('', Validators.required)
-      //  duration:this.formBuilder.control('', Validators.required),
-     //   banner:this.formBuilder.control('', Validators.required),
-      //  host:this.formBuilder.control('', Validators.required),
-      //  attendee:this.formBuilder.control('', Validators.required),
-      //  rating:this.formBuilder.control('', Validators.required),
-      //  comments:this.formBuilder.control('', Validators.required),
       });
       console.log('---->'+this.createEventForm.get('eventName').valid);
       this.nameControl = this.createEventForm.get('eventName');
@@ -88,8 +87,7 @@ export class EventCreateComponent implements OnInit{
       // .map((res: Response) => res)
       .subscribe((res)=>{
         this.hobbies = res.json();
-        console.log(this.hobbies);
-         
+        console.log(this.hobbies.length);
         if(res.status == 401){
           console.log('Invalid credentials');
           this.errorUserLogin = res.json();
@@ -113,7 +111,9 @@ export class EventCreateComponent implements OnInit{
       {
           hobArr.push({"name":hobbies[i]});
       }
-      
+      let usr:{name:string,email:string,profileIcon:string};
+      usr={name:this.user.name,email:this.user.email,profileIcon:this.user.profileIcon};
+      console.log("USR------>"+usr);
       //Get values from validated form and generate event object
 
     //  console.log("----------------->"+this.createEventForm.get('hobbyTags').value);
@@ -128,7 +128,7 @@ export class EventCreateComponent implements OnInit{
         duration: this.createEventForm.get('eventStartTime').value+"to"+this.createEventForm.get('eventEndTime').value,
         entryFee:  null,
         banner: null,
-        host: null,
+        host: usr,
         attendee:null,
         rating:null,
         comments:null
@@ -146,7 +146,7 @@ export class EventCreateComponent implements OnInit{
       const requestOptions = new RequestOptions({headers: headers});
   
       //Check proxy file for correct API call
-      this.http.post('/createEvent', sendData, requestOptions)
+      this.http.post('/postEvent', sendData, requestOptions)
         // .map((res: Response) => res)
         .subscribe((res)=>{
           this.loading = false;
@@ -168,8 +168,37 @@ export class EventCreateComponent implements OnInit{
             this._sharedService.setUser(null);
           }
         });
-  
-      // console.log(this.loginForm.get('usernameLogin').value+" <----> "+this.loginForm.get('passwordLogin').value);
-    }
+  }
+  authenticateToken(){
+    console.log("myToken : create Event"+ this.token);
+    const sendData = {
+      "generatedToken": this.token
+    };
+
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    const requestOptions = new RequestOptions({headers: headers});
+
+    //Check proxy file for correct API call
+    this.http.post('/session', sendData, requestOptions)
+      .toPromise().then((res: Response)=>{
+      console.log(res);
+      if(res.status == 200){
+        this.user = res.json().user;
+        console.log("my user" + this.user);
+        console.log("my user name " + this.user.name);
+        this.token = this.user.token;
+
+        this._sharedService.setToken(this.token);
+        this._sharedService.setUser(this.user);
+      }
+    }).catch((error)=>{
+      console.log("invalid cred -> "+error.json());
+
+      this._sharedService.setToken(' blank token ');
+      this._sharedService.setUser(null);
+    });
+  }
 
 }
